667ce1d03825971328f44807830b709cbe4c5d89
Steven Enamakel <enamakel@maha.xyz>
2025-02-04 15:38:06 +0400

    updated the buy/sell logic for the fixed curve aiming for 25% of the supply to be sold

--- contracts/curves/FixedCurve.sol
+++ contracts/curves/FixedCurve.sol
@@ -16,22 +16,29 @@ pragma solidity ^0.8.0;
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 contract FixedCurve {
-  function calculateBuy(uint256 quantityIn, uint256 raised, uint256 totalRaised)
+  uint256 public totalTokensToSell = 250_000_000 ether; // 25% of the supply to sell
+
+  function calculateBuy(uint256 tokensToBuy, uint256 assetsRaised, uint256 totalAssetRaise)
     public
-    pure
-    returns (uint256 _amountOut, uint256 _amountIn)
+    view
+    returns (uint256 _tokensOut, uint256 _assetsIn)
   {
-    uint256 remaining = totalRaised - raised;
-    uint256 amountOut = Math.min(quantityIn, remaining);
-    return (amountOut, amountOut);
+    uint256 priceE18 = totalAssetRaise * 1 ether / totalTokensToSell; // price in terms of 1 TOKEN = ? ASSET
+    uint256 tokensSold = assetsRaised * 1 ether / priceE18;
+    uint256 remainingToSell = totalTokensToSell - tokensSold;
+
+    _tokensOut = Math.min(tokensToBuy, remainingToSell);
+    _assetsIn = _tokensOut * priceE18 / 1 ether;
   }
 
-  function calculateSell(uint256 quantityOut, uint256 raised, uint256)
+  function calculateSell(uint256 tokensToSell, uint256 assetsRaised, uint256 totalAssetRaise)
     public
-    pure
-    returns (uint256 _amountOut, uint256 _amountIn)
+    view
+    returns (uint256 _assetsOut, uint256 _tokensIn)
   {
-    uint256 amountOut = Math.min(quantityOut, raised);
-    return (amountOut, amountOut);
+    uint256 priceE18 = totalAssetRaise * 1 ether / totalTokensToSell; // price in terms of 1 TOKEN = ? ASSET
+    uint256 tokensSold = assetsRaised * 1 ether / priceE18;
+    _assetsOut = tokensToSell * priceE18 / 1 ether;
+    _tokensIn = Math.min(tokensToSell, tokensSold);
   }
 }
