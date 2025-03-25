ff7b884f0d605f616bcf50a7860991897778cd5f
nikhilbajaj31 <nikhil@zerolend.xyz>
2025-03-25 19:21:25 +0530

    feat: fix pancake adapter

--- contracts/launchpad/clmm/dexes/PancakeAdapter.sol
+++ contracts/launchpad/clmm/dexes/PancakeAdapter.sol
@@ -12,33 +12,39 @@
 // Twitter: https://twitter.com/mahaxyz_
 
 pragma solidity ^0.8.0;
+import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
 
-import {ICLMMAdapter} from "contracts/interfaces/ICLMMAdapter.sol";
+import {TickMath} from "@uniswap/v4-core/src/libraries/TickMath.sol";
+import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
+import {LiquidityAmounts} from "@uniswap/v4-core/test/utils/LiquidityAmounts.sol";
+import {IWETH9} from "@uniswap/v4-periphery/src/interfaces/external/IWETH9.sol";
+import {IPancakeAdapter, PoolKey} from "contracts/interfaces/thirdparty/pancake/IPancakeAdapter.sol";
+import {ITokenTemplate} from "contracts/interfaces/ITokenTemplate.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {IClPool} from "contracts/interfaces/thirdparty/IClPool.sol";
-import {IClPoolFactory} from "contracts/interfaces/thirdparty/IClPoolFactory.sol";
+import {IPancakePool} from "contracts/interfaces/thirdparty/pancake/IPancakePool.sol";
+import { IPancakeFactory } from "contracts/interfaces/thirdparty/pancake/IPancakeFactory.sol";
 import {IPancakeSwapRouter} from "contracts/interfaces/thirdparty/pancake/IPancakeSwapRouter.sol";
-
-contract PancakeAdapter is ICLMMAdapter {
+import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+contract PancakeAdapter is IPancakeAdapter, Initializable {
     using SafeERC20 for IERC20;
 
-    IClPoolFactory public clPoolFactory;
+    IPancakeFactory public poolFactory;
     address public launchpad;
     mapping(IERC20 token => LaunchTokenParams params) public launchParams;
     address private _me;
     address public WETH9;
-    IClPool private _transientClPool;
+    IPancakePool private _transientClPool;
     IPancakeSwapRouter public swapRouter;
 
     function initialize(
         address _launchpad,
-        address _clPoolFactory,
+        address _poolFactory,
         address _swapRouter,
         address _WETH9
     ) external initializer {
         launchpad = _launchpad;
-        clPoolFactory = IClPoolFactory(_clPoolFactory);
+        poolFactory = IPancakeFactory(_poolFactory);
         swapRouter = IPancakeSwapRouter(_swapRouter);
         _me = address(this);
         WETH9 = _WETH9;
@@ -47,14 +53,14 @@ contract PancakeAdapter is ICLMMAdapter {
     function launchedTokens(
         IERC20 _token
     ) external view returns (bool launched) {
-        launched = launchParams[_token].pool != IClPool(address(0));
+        launched = launchParams[_token].pool != IPancakePool(address(0));
     }
 
     function getPool(IERC20 _token) external view returns (address pool) {
         return address(launchParams[_token].pool);
     }
 
-    /// @inheritdoc ICLMMAdapter
+    /// @inheritdoc IPancakeAdapter
     function addSingleSidedLiquidity(
         IERC20 _tokenBase,
         IERC20 _tokenQuote,
@@ -64,7 +70,7 @@ contract PancakeAdapter is ICLMMAdapter {
     ) external {
         require(msg.sender == launchpad, "!launchpad");
         require(
-            launchParams[_tokenBase].pool == IClPool(address(0)),
+            launchParams[_tokenBase].pool == IPancakePool(address(0)),
             "!launched"
         );
 
@@ -73,14 +79,14 @@ contract PancakeAdapter is ICLMMAdapter {
         uint160 sqrtPriceX961 = TickMath.getSqrtPriceAtTick(_tick1);
         uint160 sqrtPriceX962 = TickMath.getSqrtPriceAtTick(_tick2);
 
-        IClPool pool = IClPool(
-            clPoolFactory.createPool(
+        IPancakePool pool = IPancakePool(
+            poolFactory.createPool(
                 address(_tokenBase),
                 address(_tokenQuote),
-                10_000,
-                sqrtPriceX96Launch
+                10_000
             )
         );
+        pool.initialize(sqrtPriceX96Launch);
 
         {
             PoolKey memory poolKey = PoolKey({
@@ -120,7 +126,7 @@ contract PancakeAdapter is ICLMMAdapter {
         delete _transientClPool;
     }
 
-    /// @inheritdoc ICLMMAdapter
+    /// @inheritdoc IPancakeAdapter
     function swapForExactInput(
         IERC20 _tokenIn,
         IERC20 _tokenOut,
@@ -137,6 +143,7 @@ contract PancakeAdapter is ICLMMAdapter {
                 tokenOut: address(_tokenOut),
                 fee: 10_000,
                 recipient: msg.sender,
+                deadline: block.timestamp, // TODO: change to deadline
                 amountIn: _amountIn,
                 amountOutMinimum: _minAmountOut,
                 sqrtPriceLimitX96: 0
@@ -161,6 +168,7 @@ contract PancakeAdapter is ICLMMAdapter {
                 tokenOut: address(_tokenOut),
                 fee: 10_000,
                 recipient: msg.sender,
+                deadline: block.timestamp, // TODO: change to deadline
                 amountOut: _amountOut,
                 amountInMaximum: _maxAmountIn,
                 sqrtPriceLimitX96: 0
@@ -170,7 +178,7 @@ contract PancakeAdapter is ICLMMAdapter {
         _tokenIn.safeTransfer(msg.sender, _maxAmountIn - amountIn);
     }
 
-    /// @inheritdoc ICLMMAdapter
+    /// @inheritdoc IPancakeAdapter
     function claimFees(
         address _token
     ) external returns (uint256 fee0, uint256 fee1) {
@@ -199,15 +207,15 @@ contract PancakeAdapter is ICLMMAdapter {
         IERC20(params.pool.token1()).transfer(msg.sender, fee1);
     }
 
-    /// @inheritdoc ICLMMAdapter
+    /// @inheritdoc IPancakeAdapter
     function graduated(address token) external view returns (bool) {
         LaunchTokenParams memory params = launchParams[IERC20(token)];
-        if (params.pool == IClPool(address(0))) return false;
+        if (params.pool == IPancakePool(address(0))) return false;
         (, int24 tick, , , , , ) = params.pool.slot0();
         return tick >= params.tick1;
     }
 
-    function pancakeV3SwapCallback(
+    function pancakeV3MintCallback(
         uint256 amount0,
         uint256 amount1,
         bytes calldata data
@@ -223,4 +231,16 @@ contract PancakeAdapter is ICLMMAdapter {
             amount0
         );
     }
+
+    function pancakeV3SwapCallback(
+        uint256 amount0,
+        uint256 amount1,
+        bytes calldata data
+    ) external {
+        require(msg.sender == address(_transientClPool), "!clPool");    
+    }
+
+    receive() external payable {}
+    fallback() external payable {}
+    
 }
