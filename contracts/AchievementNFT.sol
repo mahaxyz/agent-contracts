0abf7919aa03fe42100b407561cfe4c821798cce
Steven Enamakel <enamakel@maha.xyz>
2025-05-26 20:09:46 +0300

    shortened the achievement nft

--- contracts/AchievementNFT.sol
+++ contracts/AchievementNFT.sol
@@ -13,108 +13,75 @@
 
 pragma solidity ^0.8.0;
 
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import {
-  ERC721EnumerableUpgradeable,
-  ERC721Upgradeable
-} from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
-
 import {IAchievementNFT} from "./interfaces/IAchievementNFT.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {IERC721Metadata} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 /**
  * @title AchievementNFT
  * @dev An NFT contract that mints achievement tokens when users complete milestones
  */
-contract AchievementNFT is IAchievementNFT, OwnableUpgradeable, ERC721EnumerableUpgradeable {
+contract AchievementNFT is IAchievementNFT, Ownable, ERC721Enumerable {
+  using Strings for string;
+  using Strings for uint256;
+
   uint256 public nextTokenId = 1;
-
-  // Minter role for authorized addresses (backend)
   mapping(address => bool) public minters;
+  mapping(uint256 => Achievement) public achievements;
+  string public baseURI;
 
-  // Token ID to achievement mapping
-  mapping(uint256 => Achievement) private _achievements;
-
-  modifier onlyMinter() {
-    require(minters[msg.sender], "AchievementNFT: caller is not a minter");
-    _;
+  constructor(string memory name_, string memory symbol_, string memory baseURI_)
+    Ownable(msg.sender)
+    ERC721(name_, symbol_)
+  {
+    baseURI = baseURI_;
   }
 
-  //@inheritdoc IAchievementNFT
-  function initialize(string memory name_, string memory symbol_, address backend) external initializer {
-    __Ownable_init(msg.sender);
-    __ERC721_init(name_, symbol_);
-    __ERC721Enumerable_init();
-    minters[backend] = true;
-  }
-
-  //@inheritdoc IAchievementNFT
-  function addMinter(address minter) external onlyOwner {
+  /// @inheritdoc IAchievementNFT
+  function toggleMinter(address minter) external onlyOwner {
     require(minter != address(0), "AchievementNFT: minter is the zero address");
     require(!minters[minter], "AchievementNFT: account is already a minter");
 
-    minters[minter] = true;
-    emit MinterAdded(minter);
+    minters[minter] = !minters[minter];
+    emit MinterToggled(minter, minters[minter]);
   }
 
-  //@inheritdoc IAchievementNFT
-  function removeMinter(address minter) external onlyOwner {
-    require(minters[minter], "AchievementNFT: account is not a minter");
-
-    minters[minter] = false;
-    emit MinterRemoved(minter);
-  }
-
-  //@inheritdoc IAchievementNFT
-  function mintAchievement(address to, Achievement memory achievement) external onlyMinter returns (uint256) {
-    return _mintSingleAchievement(to, achievement);
-  }
-
-  //@inheritdoc IAchievementNFT
-  function batchMintAchievements(address[] memory recipients, Achievement[] memory achievements)
-    external
-    onlyMinter
-    returns (uint256[] memory)
-  {
-    require(recipients.length == achievements.length, "AchievementNFT: array length mismatch");
-
-    uint256[] memory tokenIds = new uint256[](recipients.length);
-
-    for (uint256 i = 0; i < recipients.length; i++) {
-      tokenIds[i] = _mintSingleAchievement(recipients[i], achievements[i]);
-    }
-
-    return tokenIds;
-  }
-
-  //@inheritdoc IAchievementNFT
-  function getAchievement(uint256 tokenId) external view returns (Achievement memory) {
-    require(tokenId < nextTokenId, "AchievementNFT: achievement query for nonexistent token");
-    return _achievements[tokenId];
-  }
-
-  //@inheritdoc IAchievementNFT
-  function getAchievementsByOwner(address owner) external view returns (uint256[] memory) {
-    uint256 balance = balanceOf(owner);
-    uint256[] memory tokenIds = new uint256[](balance);
-
-    for (uint256 i = 0; i < balance; i++) {
-      tokenIds[i] = tokenOfOwnerByIndex(owner, i);
-    }
-
-    return tokenIds;
-  }
-
-  function _mintSingleAchievement(address to, Achievement memory achievement) internal returns (uint256) {
-    require(to != address(0), "AchievementNFT: mint to the zero address");
+  /// @inheritdoc IAchievementNFT
+  function mint(Achievement memory achievement, bytes memory signature) external returns (uint256) {
+    bytes32 messageHash = keccak256(abi.encode(msg.sender, achievement));
+    (address signer, ECDSA.RecoverError err,) = ECDSA.tryRecover(messageHash, signature);
+    require(err == ECDSA.RecoverError.NoError && minters[signer], "AchievementNFT: invalid signature");
 
     uint256 tokenId = nextTokenId++;
 
-    _safeMint(to, tokenId);
+    _safeMint(msg.sender, tokenId);
+    achievements[tokenId] = achievement;
 
-    _achievements[tokenId] = achievement;
-
-    emit AchievementMinted(to, tokenId, achievement.achievementType);
+    emit AchievementMinted(
+      msg.sender,
+      tokenId,
+      achievement.token,
+      achievement.campaignId,
+      achievement.score,
+      achievement.title,
+      achievement.description
+    );
 
     return tokenId;
+  }
+
+  function _update(address to, uint256 tokenId, address auth) internal virtual override returns (address) {
+    super._update(to, tokenId, auth);
+    // disable transfers for any user
+    require(auth == address(0), "AchievementNFT: transfers are disabled");
+    return address(0);
+  }
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
   }
 }
--- contracts/interfaces/IAchievementNFT.sol
+++ contracts/interfaces/IAchievementNFT.sol
@@ -11,75 +11,42 @@ interface IAchievementNFT {
    * @dev Achievement data structure
    */
   struct Achievement {
-    string name;
+    address token;
+    bytes32 campaignId;
+    uint256 score;
+    string title;
     string description;
-    uint256 achievementType;
   }
 
   /**
-   * @dev Emitted when a new minter is added
+   * @dev Emitted when a new minter is toggled
    */
-  event MinterAdded(address indexed minter);
-
-  /**
-   * @dev Emitted when a minter is removed
-   */
-  event MinterRemoved(address indexed minter);
+  event MinterToggled(address indexed minter, bool isMinter);
 
   /**
    * @dev Emitted when an achievement is minted
    */
-  event AchievementMinted(address indexed user, uint256 indexed tokenId, uint256 achievementType);
-
-  /**
-   * @dev Initializes the contract
-   * @param name_ Name of the NFT collection
-   * @param symbol_ Symbol of the NFT collection
-   * @param backend Address of the contract backend
-   */
-  function initialize(string memory name_, string memory symbol_, address backend) external;
+  event AchievementMinted(
+    address indexed user,
+    uint256 indexed tokenId,
+    address token,
+    bytes32 campaignId,
+    uint256 score,
+    string title,
+    string description
+  );
 
   /**
    * @dev Adds a new minter address
    * @param minter The address to add as a minter
    */
-  function addMinter(address minter) external;
-
-  /**
-   * @dev Removes a minter address
-   * @param minter The address to remove as a minter
-   */
-  function removeMinter(address minter) external;
+  function toggleMinter(address minter) external;
 
   /**
    * @dev Mints a new achievement NFT to the specified user address
-   * @param to The recipient address
    * @param achievement The achievement details
-   * @return The minted token ID
+   * @param signature The signature of the minter
+   * @return tokenId The minted token ID
    */
-  function mintAchievement(address to, Achievement memory achievement) external returns (uint256);
-
-  /**
-   * @dev Batch mint achievement NFTs to multiple users
-   * @param recipients Array of recipient addresses
-   * @param achievements Array of achievement details
-   * @return Array of minted token IDs
-   */
-  function batchMintAchievements(address[] memory recipients, Achievement[] memory achievements)
-    external
-    returns (uint256[] memory);
-
-  /**
-   * @dev Gets the achievement details for a token ID
-   * @param tokenId The token ID
-   * @return The achievement details
-   */
-  function getAchievement(uint256 tokenId) external view returns (Achievement memory);
-
-  /**
-   * @dev Gets all achievements owned by a user
-   * @param owner The owner address
-   * @return Array of token IDs owned by the user
-   */
-  function getAchievementsByOwner(address owner) external view returns (uint256[] memory);
+  function mint(Achievement memory achievement, bytes memory signature) external returns (uint256);
 }
